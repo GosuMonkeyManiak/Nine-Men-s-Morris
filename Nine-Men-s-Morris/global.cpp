52809cdf1ca31a0d1a484e81a0cd58008f4af175
Dimitar Kupanov <dimitarkupanov@gmail.com>
2025-04-22 22:50:19 +0300

    Extract the point array which contains relation between board position and their indexes on screen into global variable because it will be use in other class

---
+++ Nine-Men-s-Morris/global.cpp
@@ -0,0 +1,29 @@
+#include "global.h"
+
+point screenPositionBasedOnBoardPosition[24]
+{
+	point(0	, 0),
+	point(18, 0),
+	point(36, 0),
+	point(36, 9),
+	point(36, 18),
+	point(18, 18),
+	point(0	, 18),
+	point(0	, 9),
+	point(8	, 2),
+	point(18, 2),
+	point(28, 2),
+	point(28, 9),
+	point(28, 16),
+	point(18, 16),
+	point(8	, 16),
+	point(8	, 9),
+	point(12, 5),
+	point(18, 5),
+	point(24, 5),
+	point(24, 9),
+	point(24, 13),
+	point(18, 13),
+	point(12, 13),
+	point(12, 9)
+};
---
+++ Nine-Men-s-Morris/global.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "point.h"
+
+extern point screenPositionBasedOnBoardPosition[24];
